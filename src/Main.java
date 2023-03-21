f58d479c357b3ca3705aa5f62b6c4b01059a3c51
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 14:26:53 +0100

    Intial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Vår uppgift är att utöka klassen Person så att den även lagrar en persons barn
+        //   - Barn är Person-objekt
+        //   - Ska ha en metod nyttBarn för att registrera ett nytt barn
+        //   - Ska ha en metod antalBarn som talar om hur många barn personen har
+        //   - Ska ha en metod som skriver ut namnet på samtliga barn
+
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,58 @@
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Person {
+    // instansvariabler
+    private String förnamn;
+    private String efternamn;
+    private int föddÅr;
+    public  Person partner;
+    private List<Person> barnLista = new LinkedList<>();
+
+    // konstruktor
+    public Person(String f, String e, int å) {
+        sättFörnamn(f);
+        sättEfternamn(e);
+        sättFöddÅr(å);
+    }
+
+    // instansmetoder
+    public void sättFörnamn(String fn) {
+        if (fn != null)
+            förnamn = fn;
+    else
+        throw new NullPointerException();
+    }
+
+    public void sättEfternamn(String en) {
+        if (en != null)
+            efternamn = en;
+        else
+            throw new NullPointerException();
+    }
+
+    public void sättFöddÅr(int år) {
+        if (år > 1880 &&
+        år <= Calendar.getInstance().get(Calendar.YEAR))
+        föddÅr = år;
+    else
+        throw new IllegalArgumentException("Felaktigt �r");
+    }
+
+    public String avläsFörnamn() {
+        return förnamn;
+    }
+
+    public String avläsEfternamn() {
+        return efternamn;
+    }
+
+    public int avläsFöddÅr() {
+        return föddÅr;
+    }
+
+    public String toString() {
+        return förnamn + " " + efternamn;
+    }
+}
